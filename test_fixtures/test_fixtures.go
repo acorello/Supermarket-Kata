aba947d3ccd8d0a55eeea6633c5f32e6ab2a58fb
am <am@acorello.it>
2023-11-09 14:51:38 +0000

    Replace CSV with []struct{…}

--- test_fixtures/test_fixtures.go
+++ test_fixtures/test_fixtures.go
@@ -1,58 +1,36 @@
 package test_fixtures
 
 import (
-	"bufio"
 	_ "embed"
-	"log"
-	"strconv"
-	"strings"
 
 	"dev.acorello.it/go/supermarket-kata/item"
 	"dev.acorello.it/go/supermarket-kata/money"
 )
 
-//go:embed catalog.csv
-var csv string
-
 // this path is
-var catalog = mustFromCSV(csv)
+var catalog = makeCatalog()
 
 func Catalog() item.Catalog {
 	return catalog
 }
 
-func mustFromCSV(csvString string) item.Catalog {
-	scanner := bufio.NewScanner(strings.NewReader(csvString))
-	checkAndSkipHeader(scanner, csvString)
-	items := []item.Item{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Split(line, ",")
-		id := item.Id(strings.TrimSpace(fields[0]))
-		price, err := strconv.Atoi(strings.TrimSpace(fields[1]))
-		if err != nil {
-			log.Fatalf("failed to parse price: %v", err)
+func makeCatalog() item.Catalog {
+	items_ := make([]item.Item, len(catalog_))
+	for i, item_ := range catalog_ {
+		items_[i] = item.Item{
+			Id:    item_.Id,
+			Price: item_.Cents,
+			Unit:  item_.Unit,
 		}
-		items = append(items, item.Item{
-			Id:    id,
-			Price: money.Cents(uint(price)),
-			Unit:  strings.TrimSpace(fields[2]),
-		})
 	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("error occurred during scanning: %v", err)
-	}
-	return item.NewCatalog(items...)
+	return item.NewCatalog(items_...)
 }
 
-func checkAndSkipHeader(scanner *bufio.Scanner, path string) {
-	const HEADER = "ITEM_ID,PRICE_CENTS,UNIT"
-	read := scanner.Scan()
-	if !read {
-		log.Fatalf("empty file %q", path)
-	}
-	if scanner.Text() != HEADER {
-		log.Fatalf("expected header %q but got %q", HEADER, scanner.Text())
-	}
+var catalog_ = []struct {
+	item.Id
+	Unit string
+	money.Cents
+}{
+	{"beans", "u", 90},
+	{"oranges", "kg", 200},
 }

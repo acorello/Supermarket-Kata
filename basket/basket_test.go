009ad1e62ff0e1d21f9cd1bb382a1d637751b512
am <am@acorello.it>
2023-11-15 11:51:30 +0000

    basket_test: extract test-helpers

--- basket/basket_test.go
+++ basket/basket_test.go
@@ -21,23 +21,22 @@ var qty = must.DereFn(basket.Quantity)
 type T = *testing.T
 
 func TestBasket(t *testing.T) {
+	// a basket depends on a catalog
+	// -> verify that no basket can be created with invalid dependencies
 	t.Log("verify constructor rejects invalid arguments")
 	require.Panics(t, func() {
 		var nilCatalog basket.Catalog = nil
 		basket.NewBasket(nilCatalog)
 	}, "panic when given nil catalog")
 
+	// create a basket
 	_basket := basket.NewBasket(catalog)
 
-	t.Log("reports if an item.Id is not in its catalog")
-	{
-		itemId, err := _basket.ItemIdInCatalog("item-not-in-catalog")
-		require.Error(t, err, "invalid item.Id passed validation")
-		require.Panics(t, func() { evalAndDiscard(*itemId) },
-			"non-nil basket.itemId returned for invalid item.Id")
-	}
+	// basket method arguments must be validated using provided validation functions
+	// -> verify that validation functions reject invalid inputs
+	// -> verify that validation functions accept valid inputs
+	rejectsInvalidItemId(t, _basket)
 
-	t.Log("returns a basket.ItemId if an item.Id is in its catalog")
 	// assuming anItem is immutable: sub-tests are reading it
 	anItem := catalog.RandomItem()
 	_anItemId, err := _basket.ItemIdInCatalog(anItem.Id)
@@ -45,30 +44,8 @@ func TestBasket(t *testing.T) {
 	anItemId := *_anItemId
 
 	const minQty, maxQty = 1, 99
-	someInvalidQuantities := [...]int{math.MinInt, math.MaxInt,
-		minQty - 2, minQty - 1, maxQty + 1, maxQty + 2,
-	}
-	t.Logf("rejects quantities ( q < %d or q > %d )", minQty, maxQty)
-	for _, q := range someInvalidQuantities {
-		qty, err := basket.Quantity(q)
-
-		require.Panics(t, func() { evalAndDiscard(*qty) },
-			"returned non-nil quantity for %d", q)
-
-		require.Error(t, err,
-			"did not return an error for %d", q)
-	}
-
-	t.Logf("accepts quantities ( %d <= q <= %d )", minQty, maxQty)
-	for q := minQty; q <= maxQty; q++ {
-		qty, err := basket.Quantity(q)
-
-		require.NoError(t, err,
-			"returned an error for %d", q)
-
-		require.NotPanics(t, func() { evalAndDiscard(*qty) },
-			"returned nil quantity for %d", q)
-	}
+	rejectsInvalidQuantity(t, minQty, maxQty)
+	acceptsValidQuantity(t, minQty, maxQty)
 
 	t.Run("Total() changes as we change an item quantity", func(t T) {
 		b := _basket // make copy, we're about to run concurrently!
@@ -105,6 +82,42 @@ func TestBasket(t *testing.T) {
 		b.Remove(anItemId, qty(1))
 		require.Equal(t, zeroCents, b.Total())
 	})
+}
+
+func acceptsValidQuantity(t *testing.T, minQty int, maxQty int) {
+	t.Logf("accepts all quantities q: %d <= q <= %d", minQty, maxQty)
+	for q := minQty; q <= maxQty; q++ {
+		qty, err := basket.Quantity(q)
+
+		require.NoError(t, err,
+			"returned an error for %d", q)
+
+		require.NotPanics(t, func() { evalAndDiscard(*qty) },
+			"returned nil quantity for %d", q)
+	}
+}
+
+func rejectsInvalidQuantity(t *testing.T, minQty int, maxQty int) {
+	someInvalidQuantities := [...]int{math.MinInt, math.MaxInt,
+		minQty - 2, minQty - 1, maxQty + 1, maxQty + 2,
+	}
+	t.Logf("rejects a few invalid quantities q: q < %d or q > %d", minQty, maxQty)
+	for _, q := range someInvalidQuantities {
+		qty, err := basket.Quantity(q)
+
+		require.Panics(t, func() { evalAndDiscard(*qty) },
+			"returned non-nil quantity for %d", q)
+
+		require.Error(t, err,
+			"did not return an error for %d", q)
+	}
+}
+
+func rejectsInvalidItemId(t *testing.T, b basket.Basket) {
+	itemId, err := b.ItemIdInCatalog("item-not-in-catalog")
+	require.Error(t, err, "invalid item.Id passed validation")
+	require.Panics(t, func() { evalAndDiscard(*itemId) },
+		"non-nil basket.itemId returned for invalid item.Id")
 }
 
 func evalAndDiscard(any) {}
